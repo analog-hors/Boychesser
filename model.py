ac8d8136334e50b36a9e90d162cbbb3785738758
dsekercioglu <dsekercioglu2003@gmail.com>
2022-07-09 23:15:50 +0300

    Fix factorizer bug

--- model.py
+++ model.py
@@ -41,11 +41,13 @@ class NnHalfKP(torch.nn.Module):
             batch.nstm_indices, batch.values, (batch.size, 40960)
         )
 
+        batch.stm_indices[1][:] %= 640
+        batch.nstm_indices[1][:] %= 640
         v_board_stm_sparse = torch.sparse_coo_tensor(
-            batch.stm_indices[:] % 640, batch.values, (batch.size, 640)
+            batch.stm_indices, batch.values, (batch.size, 640)
         )
         v_board_nstm_sparse = torch.sparse_coo_tensor(
-            batch.nstm_indices[:] % 640, batch.values, (batch.size, 640)
+            batch.nstm_indices, batch.values, (batch.size, 640)
         )
 
         stm_ft = self.ft(board_stm_sparse) + self.fft(v_board_stm_sparse)
@@ -72,11 +74,13 @@ class NnHalfKA(torch.nn.Module):
             batch.nstm_indices, batch.values, (batch.size, 49152)
         )
 
+        batch.stm_indices[1][:] %= 768
+        batch.nstm_indices[1][:] %= 768
         v_board_stm_sparse = torch.sparse_coo_tensor(
-            batch.stm_indices[:] % 768, batch.values, (batch.size, 768)
+            batch.stm_indices, batch.values, (batch.size, 768)
         )
         v_board_nstm_sparse = torch.sparse_coo_tensor(
-            batch.nstm_indices[:] % 768, batch.values, (batch.size, 768)
+            batch.nstm_indices, batch.values, (batch.size, 768)
         )
 
         stm_ft = self.ft(board_stm_sparse) + self.fft(v_board_stm_sparse)

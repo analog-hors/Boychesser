68fe4bc395465e30bb8fe9e1cec27c30256a7c06
dsekercioglu <dsekercioglu2003@gmail.com>
2022-07-11 20:12:56 +0300

    Convert gets NN path as argument

--- convert/src/main.rs
+++ convert/src/main.rs
@@ -2,7 +2,11 @@ mod halfkp;
 mod utils;
 use halfkp::HalfKp;
 fn main() {
-    let content = std::fs::read("../nn/nn.json").unwrap();
+    let mut path = None;
+    for arg in std::env::args().into_iter().skip(1).take(1) {
+        path = Some(arg);
+    }
+    let content = std::fs::read(format!("../nn/{}.json", path.unwrap())).unwrap();
 
     let arch = HalfKp::from(&content);
     let bin = arch.to_bin(255.0, 64.0);

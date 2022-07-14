72d26170f7db864a9cebb0f97dbf6d2b245b281d
doruk <dsekercioglu2003@gmail.com>
2022-07-14 08:46:55 +0300

    Use try from to crash on overflow

--- convert/src/utils.rs
+++ convert/src/utils.rs
@@ -1,31 +1,35 @@
 pub fn serialize_dense_i8(weights: &[Box<[f32]>], bin: &mut Vec<u8>, scale: f32) {
     for weights in weights {
         for &weight in weights.iter() {
-            assert!((weight * scale) <= 127.0 && (weight * scale) >= -128.0, "{}", weight * scale);
-            bin.extend(((weight * scale) as i8).to_le_bytes())
+            bin.extend(i8::try_from((weight * scale) as i64).unwrap().to_le_bytes())
         }
     }
 }
 
 pub fn serialize_flat_i8(weights: &[f32], bin: &mut Vec<u8>, scale: f32) {
-    for weight in weights.iter() {
-        assert!((weight * scale) <= 127.0 && (weight * scale) >= -128.0, "{}", weight * scale);
-        bin.extend(((*weight * scale) as i8).to_le_bytes())
+    for &weight in weights {
+        bin.extend(i8::try_from((weight * scale) as i64).unwrap().to_le_bytes())
     }
 }
 
 pub fn serialize_dense_i16(weights: &[Box<[f32]>], bin: &mut Vec<u8>, scale: f32) {
     for weights in weights {
         for &weight in weights.iter() {
-            assert!((weight * scale) <= 32767.0 && (weight * scale) >= -32768.0, "{}", weight * scale);
-            bin.extend(((weight * scale) as i16).to_le_bytes())
+            bin.extend(
+                i16::try_from((weight * scale) as i64)
+                    .unwrap()
+                    .to_le_bytes(),
+            )
         }
     }
 }
 
 pub fn serialize_flat_i16(weights: &[f32], bin: &mut Vec<u8>, scale: f32) {
-    for weight in weights.iter() {
-        assert!((weight * scale) <= 32767.0 && (weight * scale) >= -32768.0, "{}", weight * scale);
-        bin.extend(((*weight * scale) as i16).to_le_bytes())
+    for &weight in weights {
+        bin.extend(
+            i16::try_from((weight * scale) as i64)
+                .unwrap()
+                .to_le_bytes(),
+        )
     }
 }

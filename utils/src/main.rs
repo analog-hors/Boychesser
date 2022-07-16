e38e06d265e76c516b63343e43c0148fe955578c
MinusKelvin <mark.carlson@minuskelvin.net>
2022-07-17 00:38:12 +1000

    add utility to randomly interleave datasets

--- utils/src/main.rs
+++ utils/src/main.rs
@@ -1,17 +1,20 @@
 use structopt::StructOpt;
 
 mod convert;
+mod interleave;
 mod txt_to_data;
 
 #[derive(StructOpt)]
 pub enum Options {
     Convert(convert::Options),
+    Interleave(interleave::Options),
     TxtToData(txt_to_data::Options),
 }
 
 fn main() {
     match Options::from_args() {
         Options::Convert(options) => convert::run(options),
+        Options::Interleave(options) => interleave::run(options).unwrap(),
         Options::TxtToData(options) => txt_to_data::run(options).unwrap(),
     }
 }

4a5f13db100592406ba6ab18cf9fa1bc3715b028
MinusKelvin <mark.carlson@minuskelvin.net>
2022-07-17 00:33:38 +1000

    add simple utility for converting old text format to marlinformat

--- utils/src/main.rs
+++ utils/src/main.rs
@@ -1,14 +1,17 @@
 use structopt::StructOpt;
 
 mod convert;
+mod txt_to_data;
 
 #[derive(StructOpt)]
 pub enum Options {
     Convert(convert::Options),
+    TxtToData(txt_to_data::Options),
 }
 
 fn main() {
     match Options::from_args() {
         Options::Convert(options) => convert::run(options),
+        Options::TxtToData(options) => txt_to_data::run(options).unwrap(),
     }
 }

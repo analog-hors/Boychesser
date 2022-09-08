8f4ec1ad98e337d049a20253360ca114433e5a1c
MinusKelvin <mark.carlson@minuskelvin.net>
2022-09-09 09:48:38 +1000

    tee to log file

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ rm -r nn data
 mkdir -p nn data
 zstd -d "/content/drive/MyDrive/datasets/$1.bin.zst" -o data/data.bin
 
-python3 trainer/main.py --data-root data --scale 1016 --save-epochs 5 --lr 0.001 --epochs 50
+python3 trainer/main.py --data-root data --scale 1016 --save-epochs 5 --lr 0.001 --epochs 50 | tee nn/log
 
 tar cf networks.tar -C nn .
 zstd networks.tar -o "/content/drive/MyDrive/networks/$TRAIN_ID.tar.zst"

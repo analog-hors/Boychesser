87bcbf1a65b1d78e3a9ddc846f33a72cc1d25f18
MinusKelvin <mark.carlson@minuskelvin.net>
2022-09-09 09:30:13 +1000

    make a flat archive

--- train.sh
+++ train.sh
@@ -8,5 +8,5 @@ zstd -d "/content/drive/MyDrive/datasets/$1.bin.zst" -o data/data.bin
 
 python3 trainer/main.py --data-root data --scale 1016 --save-epochs 5 --lr 0.001 --epochs 50
 
-tar cf networks.tar nn
-zstd networks.tar -o "/content/drive/MyDrive/networks/$TRAIN_ID.bin.zst"
+tar cf networks.tar -C nn .
+zstd networks.tar -o "/content/drive/MyDrive/networks/$TRAIN_ID.tar.zst"

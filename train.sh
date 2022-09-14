438ff8c61f00b7b84b7ae93b90abf5fab260f9dd
MinusKelvin <mark.carlson@minuskelvin.net>
2022-09-14 13:47:04 +1000

    enhance train.sh

--- train.sh
+++ train.sh
@@ -3,11 +3,13 @@ set -e
 
 TRAIN_ID="$(date -u '+%F-%H%M%S')-$1"
 
-rm -r nn data
-mkdir -p nn data
+[ -e nn ] && rm -r nn; mkdir nn
+[ -e data ] && rm -r data; mkdir data
 zstd -d "/content/drive/MyDrive/datasets/$1.bin.zst" -o data/data.bin
 
-python3 trainer/main.py --data-root data --scale 1016 --save-epochs 5 --lr 0.001 --epochs 50 | tee nn/log
+python3 trainer/main.py --data-root data --scale 1016 --save-epochs 1 \
+    --lr 0.001 --epochs 50 --wdl 0 \
+    | tee nn/log
 
 tar cf networks.tar -C nn .
 zstd networks.tar -o "/content/drive/MyDrive/networks/$TRAIN_ID.tar.zst"

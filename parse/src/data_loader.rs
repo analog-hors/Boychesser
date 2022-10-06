3c1c965f509621e301394ff1b2fb9a76e56be1f8
MinusKelvin <mark.carlson@minuskelvin.net>
2022-10-06 19:26:51 +1100

    redo parallelization

--- parse/src/data_loader.rs
+++ parse/src/data_loader.rs
@@ -1,6 +1,5 @@
 use std::io::{Seek, SeekFrom};
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
-use std::sync::Mutex;
 use std::{fs::File, io::Read, path::Path};
 
 use bytemuck::Zeroable;
@@ -11,6 +10,8 @@ use rayon::prelude::*;
 use crate::batch::Batch;
 use crate::bucketing::*;
 use crate::input_features::*;
+
+const BUFFERED_BATCHES: usize = 64;
 
 pub struct BatchReader {
     recv: Receiver<Box<Batch>>,
@@ -27,7 +28,7 @@ impl BatchReader {
         let mut file = File::open(path)?;
         let dataset_size = file.seek(SeekFrom::End(0))? / std::mem::size_of::<PackedBoard>() as u64;
         file.seek(SeekFrom::Start(0))?;
-        let (send, recv) = sync_channel(4);
+        let (send, recv) = sync_channel(BUFFERED_BATCHES);
         std::thread::spawn(move || {
             dataloader_thread(send, file, feature_format, bucketing_scheme, batch_size)
         });
@@ -50,7 +51,8 @@ fn dataloader_thread(
     bucketing_scheme: BucketingSchemeType,
     batch_size: usize,
 ) {
-    let mut board_buffer = vec![PackedBoard::zeroed(); batch_size];
+    let mut board_buffer = vec![PackedBoard::zeroed(); batch_size * BUFFERED_BATCHES];
+    let mut batches = vec![];
     loop {
         let buffer = bytemuck::cast_slice_mut(&mut board_buffer);
         let mut bytes_read = 0;
@@ -67,77 +69,82 @@ fn dataloader_thread(
         }
         let boards = &board_buffer[..elems];
 
-        let batch = match feature_format {
-            InputFeatureSetType::Board768 => match bucketing_scheme {
-                BucketingSchemeType::NoBucketing => {
-                    process::<Board768, NoBucketing>(batch_size, boards)
-                }
-                BucketingSchemeType::ModifiedMaterial => {
-                    process::<Board768, ModifiedMaterial>(batch_size, boards)
-                }
-                BucketingSchemeType::PieceCount => {
-                    process::<Board768, PieceCount>(batch_size, boards)
-                }
-            },
-            InputFeatureSetType::HalfKp => match bucketing_scheme {
-                BucketingSchemeType::NoBucketing => {
-                    process::<HalfKp, NoBucketing>(batch_size, boards)
-                }
-                BucketingSchemeType::ModifiedMaterial => {
-                    process::<HalfKp, ModifiedMaterial>(batch_size, boards)
-                }
-                BucketingSchemeType::PieceCount => {
-                    process::<HalfKp, PieceCount>(batch_size, boards)
-                }
-            },
-            InputFeatureSetType::HalfKa => match bucketing_scheme {
-                BucketingSchemeType::NoBucketing => {
-                    process::<HalfKa, NoBucketing>(batch_size, boards)
-                }
-                BucketingSchemeType::ModifiedMaterial => {
-                    process::<HalfKa, ModifiedMaterial>(batch_size, boards)
-                }
-                BucketingSchemeType::PieceCount => {
-                    process::<HalfKa, PieceCount>(batch_size, boards)
-                }
-            },
-            InputFeatureSetType::Board768Cuda => match bucketing_scheme {
-                BucketingSchemeType::NoBucketing => {
-                    process::<Board768Cuda, NoBucketing>(batch_size, boards)
-                }
-                BucketingSchemeType::ModifiedMaterial => {
-                    process::<Board768Cuda, ModifiedMaterial>(batch_size, boards)
-                }
-                BucketingSchemeType::PieceCount => {
-                    process::<Board768Cuda, PieceCount>(batch_size, boards)
-                }
-            },
-            InputFeatureSetType::HalfKpCuda => match bucketing_scheme {
-                BucketingSchemeType::NoBucketing => {
-                    process::<HalfKpCuda, NoBucketing>(batch_size, boards)
-                }
-                BucketingSchemeType::ModifiedMaterial => {
-                    process::<HalfKpCuda, ModifiedMaterial>(batch_size, boards)
-                }
-                BucketingSchemeType::PieceCount => {
-                    process::<HalfKpCuda, PieceCount>(batch_size, boards)
-                }
-            },
-            InputFeatureSetType::HalfKaCuda => match bucketing_scheme {
-                BucketingSchemeType::NoBucketing => {
-                    process::<HalfKaCuda, NoBucketing>(batch_size, boards)
-                }
-                BucketingSchemeType::ModifiedMaterial => {
-                    process::<HalfKaCuda, ModifiedMaterial>(batch_size, boards)
-                }
-                BucketingSchemeType::PieceCount => {
-                    process::<HalfKaCuda, PieceCount>(batch_size, boards)
-                }
-            },
-        };
+        boards
+            .par_chunks(batch_size)
+            .map(|boards| match feature_format {
+                InputFeatureSetType::Board768 => match bucketing_scheme {
+                    BucketingSchemeType::NoBucketing => {
+                        process::<Board768, NoBucketing>(batch_size, boards)
+                    }
+                    BucketingSchemeType::ModifiedMaterial => {
+                        process::<Board768, ModifiedMaterial>(batch_size, boards)
+                    }
+                    BucketingSchemeType::PieceCount => {
+                        process::<Board768, PieceCount>(batch_size, boards)
+                    }
+                },
+                InputFeatureSetType::HalfKp => match bucketing_scheme {
+                    BucketingSchemeType::NoBucketing => {
+                        process::<HalfKp, NoBucketing>(batch_size, boards)
+                    }
+                    BucketingSchemeType::ModifiedMaterial => {
+                        process::<HalfKp, ModifiedMaterial>(batch_size, boards)
+                    }
+                    BucketingSchemeType::PieceCount => {
+                        process::<HalfKp, PieceCount>(batch_size, boards)
+                    }
+                },
+                InputFeatureSetType::HalfKa => match bucketing_scheme {
+                    BucketingSchemeType::NoBucketing => {
+                        process::<HalfKa, NoBucketing>(batch_size, boards)
+                    }
+                    BucketingSchemeType::ModifiedMaterial => {
+                        process::<HalfKa, ModifiedMaterial>(batch_size, boards)
+                    }
+                    BucketingSchemeType::PieceCount => {
+                        process::<HalfKa, PieceCount>(batch_size, boards)
+                    }
+                },
+                InputFeatureSetType::Board768Cuda => match bucketing_scheme {
+                    BucketingSchemeType::NoBucketing => {
+                        process::<Board768Cuda, NoBucketing>(batch_size, boards)
+                    }
+                    BucketingSchemeType::ModifiedMaterial => {
+                        process::<Board768Cuda, ModifiedMaterial>(batch_size, boards)
+                    }
+                    BucketingSchemeType::PieceCount => {
+                        process::<Board768Cuda, PieceCount>(batch_size, boards)
+                    }
+                },
+                InputFeatureSetType::HalfKpCuda => match bucketing_scheme {
+                    BucketingSchemeType::NoBucketing => {
+                        process::<HalfKpCuda, NoBucketing>(batch_size, boards)
+                    }
+                    BucketingSchemeType::ModifiedMaterial => {
+                        process::<HalfKpCuda, ModifiedMaterial>(batch_size, boards)
+                    }
+                    BucketingSchemeType::PieceCount => {
+                        process::<HalfKpCuda, PieceCount>(batch_size, boards)
+                    }
+                },
+                InputFeatureSetType::HalfKaCuda => match bucketing_scheme {
+                    BucketingSchemeType::NoBucketing => {
+                        process::<HalfKaCuda, NoBucketing>(batch_size, boards)
+                    }
+                    BucketingSchemeType::ModifiedMaterial => {
+                        process::<HalfKaCuda, ModifiedMaterial>(batch_size, boards)
+                    }
+                    BucketingSchemeType::PieceCount => {
+                        process::<HalfKaCuda, PieceCount>(batch_size, boards)
+                    }
+                },
+            })
+            .collect_into_vec(&mut batches);
 
-        if send.send(batch).is_err() {
-            break;
+        for batch in batches.drain(..) {
+            if send.send(batch).is_err() {
+                break;
+            }
         }
     }
 }
@@ -146,13 +153,13 @@ fn process<F: InputFeatureSet, B: BucketingScheme>(
     batch_size: usize,
     boards: &[PackedBoard],
 ) -> Box<Batch> {
-    let batch = Mutex::new(Box::new(Batch::new(
+    let mut batch = Box::new(Batch::new(
         batch_size,
         F::MAX_FEATURES,
         F::INDICES_PER_FEATURE,
-    )));
+    ));
 
-    let foreach = |packed: &PackedBoard| {
+    for packed in boards {
         (|| {
             let (board, cp, wdl, _) = packed.unpack()?;
             let cp = cp as f32;
@@ -163,14 +170,12 @@ fn process<F: InputFeatureSet, B: BucketingScheme>(
                 Color::Black => (-cp, 1.0 - wdl),
             };
 
-            let mut batch = batch.lock().unwrap();
             let entry = batch.make_entry(cp, wdl, B::bucket(&board));
             F::add_features(board, entry);
 
             Some(())
         })();
-    };
+    }
 
-    boards.iter().for_each(foreach);
-    batch.into_inner().unwrap()
+    batch
 }

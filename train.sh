256159fdcffcb0f00d907e6767f0c55a0ee3af2c
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-08 12:39:53 +1100

    increase game outcome influence

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ TRAIN_ID="$(date -u '+%F-%H%M%S')-$1"
 NETS=`mktemp -d`
 
 python3 trainer/main.py --nndir "$NETS" --data "$1.bin" --scale 1016 --save-epochs 1 \
-    --lr 0.001 --epochs 45 --lr-drop 30 --wdl 0.1 \
+    --lr 0.001 --epochs 45 --lr-drop 30 --wdl 0.5 \
     | tee /dev/stderr >"$NETS"/log
 
 pushd "$NETS" >/dev/null

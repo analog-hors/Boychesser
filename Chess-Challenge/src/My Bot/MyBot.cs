584b19dcf4532fa99a44533b11007964ec02b927
Algorhythm-sxv <sebastian.venter@gmail.com>
2023-07-22 22:21:42 +0200

    add TT move ordering
    bench: 44750
    tokens: 610

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,14 +2,16 @@
 using System;
 
 // This struct should be 14 bytes large, which with padding means 16 bytes
-struct TtEntry {
+struct TtEntry
+{
     public ulong hash;
     public short score;
     public ushort moveRaw;
     public byte depth, bound /* BOUND_EXACT=1, BOUND_LOWER=2, BOUND_UPPER=3 */;
 }
 
-public class MyBot : IChessBot {
+public class MyBot : IChessBot
+{
 
     public long nodes = 0;
     public int maxSearchTime;
@@ -17,20 +19,25 @@ public class MyBot : IChessBot {
     // Assuming the size of TtEntry is indeed 16 bytes, this table is precisely 256MiB.
     TtEntry[] transposition_table = new TtEntry[0x1000000];
 
-    public Move Think(Board board, Timer timer) {
+    public Move Think(Board board, Timer timer)
+    {
         nodes = 0;
         maxSearchTime = timer.MillisecondsRemaining / 80;
 
         Move best = Move.NullMove;
 
-        for (int depth = 1; depth <= 200; depth++) {
+        for (int depth = 1; depth <= 200; depth++)
+        {
             //If score is of this value search has been aborted, DO NOT use result
-            try {
+            try
+            {
                 (int score, Move bestMove) = Negamax(board, -999999, 999999, depth, timer, depth, 0);
                 best = bestMove;
                 //Use for debugging, commented out because it saves a LOT of tokens!!
                 //Console.WriteLine("info depth " + depth + " score cp " + score);
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 break;
             }
         }
@@ -38,9 +45,11 @@ public class MyBot : IChessBot {
         return best;
     }
 
-    public (int, Move) Negamax(Board board, int alpha, int beta, int depth, Timer timer, int searchingDepth, int ply) {
+    public (int, Move) Negamax(Board board, int alpha, int beta, int depth, Timer timer, int searchingDepth, int ply)
+    {
         //abort search
-        if (timer.MillisecondsElapsedThisTurn >= maxSearchTime && searchingDepth > 1) {
+        if (timer.MillisecondsElapsedThisTurn >= maxSearchTime && searchingDepth > 1)
+        {
             throw new Exception();
         }
 
@@ -48,18 +57,28 @@ public class MyBot : IChessBot {
         nodes++;
 
         // check for game end
-        if (board.IsInCheckmate()) {
+        if (board.IsInCheckmate())
+        {
             return (-30000, Move.NullMove);
         }
 
         ref var tt = ref transposition_table[board.ZobristKey % 0x1000000];
         bool tt_good = tt.hash == board.ZobristKey;
 
-        if (tt_good && tt.depth >= depth && ply > 0) {
-            if (tt.bound == 1 /* BOUND_EXACT */ ||
+        ushort ttRawMove = 0;
+        if (tt_good)
+        {
+            // tt cutoff
+            if (tt.depth >= depth && ply > 0 && tt.bound == 1 /* BOUND_EXACT */ ||
                     tt.bound == 2 /* BOUND_LOWER */ && tt.score >= beta ||
-                    tt.bound == 3 /* BOUND_UPPER */ && tt.score <= alpha) {
+                    tt.bound == 3 /* BOUND_UPPER */ && tt.score <= alpha)
+            {
                 return (tt.score, Move.NullMove);
+            }
+            // tt move ordering
+            else
+            {
+                ttRawMove = tt.moveRaw;
             }
         }
 
@@ -68,17 +87,20 @@ public class MyBot : IChessBot {
 
         // static eval for qsearch
         int staticEval;
-        if (depth <= 0) {
+        if (depth <= 0)
+        {
             PieceList[] pieceLists = board.GetAllPieceLists();
             staticEval = (pieceLists[0].Count - pieceLists[6].Count)
                 + 3 * (pieceLists[1].Count + pieceLists[2].Count - pieceLists[7].Count - pieceLists[8].Count)
                 + 5 * (pieceLists[3].Count - pieceLists[9].Count)
                 + 9 * (pieceLists[4].Count - pieceLists[10].Count);
             staticEval = board.IsWhiteToMove ? staticEval : -staticEval;
-            if (staticEval >= beta) {
+            if (staticEval >= beta)
+            {
                 return (staticEval, Move.NullMove);
             }
-            if (staticEval > alpha) {
+            if (staticEval > alpha)
+            {
                 raisedAlpha = true;
                 alpha = staticEval;
             }
@@ -87,28 +109,35 @@ public class MyBot : IChessBot {
 
         var moves = board.GetLegalMoves(depth <= 0);
         var scores = new int[moves.Length];
-        for (int i = 0; i < moves.Length; i++) {
+        for (int i = 0; i < moves.Length; i++)
+        {
             // sort moves MVV-LVA
             scores[i] = tt_good && moves[i].RawValue == tt.moveRaw ? 10000 :
                 (int)moves[i].CapturePieceType * 8 - (int)moves[i].MovePieceType;
+            // big bonus for TT move
+            scores[i] += moves[i].RawValue == ttRawMove ? 10000 : 0;
             scores[i] *= -1;
         }
 
         Array.Sort(scores, moves);
         Move bestMove = Move.NullMove;
-        foreach (Move move in moves) {
+        foreach (Move move in moves)
+        {
             board.MakeMove(move);
             var score = board.IsDraw() ? 0 : -Negamax(board, -beta, -alpha, depth - 1, timer, searchingDepth, ply + 1).Item1;
             board.UndoMove(move);
 
-            if (score > bestScore) {
+            if (score > bestScore)
+            {
                 bestScore = score;
                 bestMove = move;
             }
-            if (score >= beta) {
+            if (score >= beta)
+            {
                 break;
             }
-            if (score > alpha) {
+            if (score > alpha)
+            {
                 raisedAlpha = true;
                 alpha = score;
             }

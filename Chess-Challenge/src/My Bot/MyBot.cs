ae534faa614b1ab52a043573de6f1bafd402bc24
MinusKelvin <mark.carlson@minuskelvin.net>
2023-07-24 12:08:30 +1000

    make it so we don't search a move we're ignoring due to lmp anyways
    bench: 3556640
    tokens: 997

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -139,6 +139,10 @@ public class MyBot : IChessBot {
         Move bestMove = nullMove;
         int moveCount = 0, score;
         foreach (Move move in moves) {
+            //LMP
+            if (nonPv && depth <= 4 && moveCount > 15 * depth && !move.IsCapture)
+                break;
+
             board.MakeMove(move);
             int nextDepth = board.IsInCheck() ? depth : depth - 1;
             if (board.IsDraw())
@@ -156,10 +160,6 @@ public class MyBot : IChessBot {
             }
 
             board.UndoMove(move);
-
-            //LMP
-            if (nonPv && depth <= 4 && moveCount > 15 * depth && !move.IsCapture)
-                break;
 
             if (score > bestScore) {
                 bestScore = score;

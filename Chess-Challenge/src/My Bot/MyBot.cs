63b5d38e54b3295944a839b4152a5a4ce89f7240
MinusKelvin <mark.carlson@minuskelvin.net>
2023-07-30 17:58:31 +1000

    don't count high-history quiets for lmp
    bench: 1512839
    tokens: 1012

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -167,7 +167,7 @@ public class MyBot : IChessBot {
         Move bestMove = nullMove;
         foreach (Move move in moves) {
             //LMP
-            if (nonPv && depth <= 4 && !move.IsCapture && quietsToCheck-- == 0)
+            if (nonPv && depth <= 4 && scores[moveCount] > -256 && quietsToCheck-- == 0)
                 break;
 
             board.MakeMove(move);

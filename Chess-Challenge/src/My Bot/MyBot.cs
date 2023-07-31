8cf79eac2933987bb0b3b2c8d92ef3d174dab936
dsekercioglu <dsekercioglu2003@gmail.com>
2023-07-31 18:03:58 +0200

    history margin proportional to depth
    bench: 7612220
    tokens: 1024

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -171,7 +171,7 @@ public class MyBot : IChessBot {
         Move bestMove = nullMove;
         foreach (Move move in moves) {
             //LMP
-            if (nonPv && depth <= 4 && !move.IsCapture && (quietsToCheck-- == 0 || (scores[moveCount] > 256 || eval + 300 * depth < alpha) && moveCount != 0))
+            if (nonPv && depth <= 4 && !move.IsCapture && (quietsToCheck-- == 0 || (scores[moveCount] > 128 * depth || eval + 300 * depth < alpha) && moveCount != 0))
                 break;
 
             board.MakeMove(move);

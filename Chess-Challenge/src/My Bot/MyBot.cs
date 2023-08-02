4e1e3652f5947cbb05352a7aaf9cfdb8f9aa996f
MinusKelvin <mark.carlson@minuskelvin.net>
2023-08-02 15:56:40 +1000

    history reduction without history clearing
    bench: 5674767
    tokens: 1032

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -185,7 +185,7 @@ public class MyBot : IChessBot {
             else {
                 // use tmp as reduction
                 tmp = move.IsCapture || nextDepth >= depth ? 0
-                    : (moveCount * 59 + depth * 109) / 1000 + Min(moveCount / 6, 1);
+                    : Max(0, (moveCount * 59 + depth * 109) / 1000 + Min(moveCount / 6, 1) - HistoryValue(move) / 200);
                 score = -Negamax(~alpha, -alpha, nextDepth - tmp, nextPly);
                 if (score > alpha && tmp != 0)
                     score = -Negamax(~alpha, -alpha, nextDepth, nextPly);

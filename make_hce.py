ae5c88d884519986065cce86729a09a315dce3a7
analog-hors <44050761+analog-hors@users.noreply.github.com>
2023-08-06 21:50:10 +1000

    update make_hce script to output new layout

--- make_hce.py
+++ make_hce.py
@@ -11,14 +11,9 @@ features = len(raw_weights) // 2
 mg_weights = raw_weights[:features]
 eg_weights = raw_weights[features:]
 weights = zip(mg_weights, eg_weights)
-pst_deltas: list[int] = []
-eval_consts: list[int] = []
 
 def take(n: int) -> list[tuple[float, float]]:
     return [next(weights) for _ in range(n)]
-
-def pos() -> int:
-    return len(pst_deltas) * 2 + len(eval_consts)
 
 def quantize_i16(n: float) -> int:
     n = round(n * EVAL_SCALE)
@@ -36,24 +31,20 @@ def quantize_u8(n: float) -> int:
 def packed_eval(mg: float, eg: float) -> int:
     return quantize_i16(eg) * 0x10000 + quantize_i16(mg)
 
-def add_weights(name: str, weights: list[tuple[float, float]]):
-    print(f"{name} offset: {pos()}")
-    for mg, eg in weights:
-        eval_consts.append(packed_eval(mg, eg))
+def encode_weights(weights: list[tuple[float, float]]) -> list[int]:
+    return [packed_eval(mg, eg) for mg, eg in weights]
 
 def print_weight(name: str, weight: tuple[float, float]):
     value = packed_eval(weight[0], weight[1])
     print(f"{name} value: {value:#010x}")
 
-def add_psts(psts: list[list[tuple[float, float]]], material_weights: list[tuple[float, float]]):
-    print("pst offset: 0")
-    print(f"material offset: {len(psts) * 8 * 2}")
-    assert len(pst_deltas) == 0
-    assert len(eval_consts) == 0
-    assert len(psts) == len(material_weights)
-    for pst, (mg_mat, eg_mat) in zip(psts, material_weights):
+def encode_psts_and_material(psts: list[list[tuple[float, float]]], material: list[tuple[float, float]]) -> tuple[list[int], list[int]]:
+    assert len(psts) == len(material)
+    material_consts = []
+    pst_deltas = []
+    for pst, (mg_mat, eg_mat) in zip(psts, material):
         assert len(pst) == 32
-        eval_consts.append(packed_eval(mg_mat, eg_mat))
+        material_consts.append(packed_eval(mg_mat, eg_mat))
         mg_error = quantize_i16_error(mg_mat)
         eg_error = quantize_i16_error(eg_mat)
         for i in range(0, 32, 4):
@@ -61,6 +52,11 @@ def add_psts(psts: list[list[tuple[float, float]]], material_weights: list[tuple
             (am, ae), (bm, be), (cm, ce), (dm, de) = w
             b = [de, ce, dm, cm, be, ae, bm, am]
             pst_deltas.append(int.from_bytes(b, "big"))
+    # shift all values to the right (removing the redundant king value)
+    # this layout saves tokens in the eval code
+    material_consts.insert(0, 0)
+    material_consts.pop()
+    return pst_deltas, material_consts
 
 pawn_pst = take(32)
 knight_pst = take(32)
@@ -74,18 +70,38 @@ tempo = take(1)
 own_pawns_file = take(6)
 for _ in weights: assert False
 
-add_psts([pawn_pst, knight_pst, bishop_pst, rook_pst, queen_pst, king_pst], material)
-add_weights("mobility", mobility)
-add_weights("own_pawns_file", own_pawns_file)
 print_weight("tempo", *tempo)
+all_psts = [pawn_pst, knight_pst, bishop_pst, rook_pst, queen_pst, king_pst]
+pst_deltas, material_consts = encode_psts_and_material(all_psts, material)
+mobility_consts = encode_weights(mobility)
+own_pawns_file_consts = encode_weights(own_pawns_file)
 
-packed_eval_consts = [*pst_deltas]
-for i in range(0, len(eval_consts), 2):
-    lo = eval_consts[i]
-    hi = eval_consts[i + 1] if i + 1 < len(eval_consts) else 0
-    packed_eval_consts.append((hi % 2 ** 32) << 32 | (lo % 2 ** 32))
-print("ulong[] packedEvalConsts = {", end="")
-for i, n in enumerate(packed_eval_consts):
+packed_data = []
+
+def add_i32_data(all_consts: list[tuple[str, list[int]]]):
+    flattened = []
+    for name, consts in all_consts:
+        offset = len(packed_data) * 2 + len(flattened)
+        print(f"{name} offset: {offset} (i32 data)")
+        flattened.extend(consts)
+    for i in range(0, len(flattened), 2):
+        lo = flattened[i]
+        hi = flattened[i + 1] if i + 1 < len(flattened) else 0
+        packed_data.append((hi % 2 ** 32) << 32 | (lo % 2 ** 32))
+
+def add_u64_data(name: str, consts: list[int]):
+    print(f"{name} offset: {len(packed_data)} (u64 data)")
+    packed_data.extend(consts)
+
+add_i32_data([
+    ("material", material_consts),
+    ("mobility", mobility_consts),
+    ("own_pawns_file", own_pawns_file_consts),
+])
+add_u64_data("psts", pst_deltas)
+
+print("ulong[] packedData = {", end="")
+for i, n in enumerate(packed_data):
     if i % 4 == 0:
         print()
         print("   ", end="")

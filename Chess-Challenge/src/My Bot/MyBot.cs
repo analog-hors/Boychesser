b2e04c7760ae3ee5ce91c7778995df9894989828
analog-hors <44050761+analog-hors@users.noreply.github.com>
2023-08-07 13:19:15 +1000

    use fewer tokens for move ordering
    bench: 5857625
    tokens: 1016

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -166,9 +166,8 @@ public class MyBot : IChessBot {
         tmp = 0;
         foreach (Move move in moves)
             // sort capture moves by MVV-LVA, quiets by history, and hashmove first
-            scores[tmp++] -= ttHit && move.RawValue == tt.Item2 /* moveRaw */ ? 10000
-                : move.IsCapture ? (int)move.CapturePieceType * 8 - (int)move.MovePieceType + 5000
-                : HistoryValue(move);
+            scores[tmp++] -= ttHit && move.RawValue == tt.Item2 /* moveRaw */ ? 100000
+                : Max((int)move.CapturePieceType * 4096 - (int)move.MovePieceType - 2048, HistoryValue(move));
         // end tmp use
 
         Array.Sort(scores, moves);

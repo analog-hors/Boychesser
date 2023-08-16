4258db62f5e840501ba70f159f26bfc3e94f0290
MinusKelvin <mark.carlson@minuskelvin.net>
2023-08-16 17:56:33 +1000

    test mobility eval for elo
    bench: 5765679
    tokens: 999

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -25,22 +25,22 @@ public class MyBot : IChessBot {
     int[,,] history = new int[2, 7, 64];
 
     ulong[] packedData = {
-        0x0000000000000000, 0x292f1611332f1801, 0x2222241c2d25190a, 0x1a232e21312a1809,
-        0x293336263b3d2912, 0x5b614f4671673821, 0x96a37664b1ad5d59, 0x0002000300060005,
-        0xfffcfffe00040002, 0x32260223271d2b09, 0x26210f1c221b2c11, 0x1e1f1a232c211b0b,
-        0x2832282d3d372a10, 0x60683c4a856d341b, 0x9fc75f45ddcc0722, 0x0000000000000000,
-        0x5e5549463d2e3e36, 0x6d64575860534944, 0x8376685f6e585b44, 0x8e8c6d6a78696554,
-        0x978e787e826b6967, 0x8083a29470608a63, 0x796b7f836c535852, 0x67795e0669050200,
-        0x3939302b3b353434, 0x3b382f3938343e3e, 0x424032363f453b35, 0x3a423d30443c303b,
-        0x3e3f443e4a443836, 0x3941504b464a4f41, 0x46443033473f302a, 0x575401004d4f0d13,
-        0x868c68608d8b5a57, 0x8b885d5c888e5444, 0x919257559093584b, 0x9a9d5c599d9d514f,
-        0x9fa36d6ba3a3635f, 0x9ca18a809ea3846e, 0xa3a49392aaa3727b, 0xa1a38f92a1a18c8b,
-        0xa89a7a79a2b6776f, 0xaaa280819fab7e7b, 0xc5cc7679c9bc7a7b, 0xece46f73e1d57779,
-        0xfff77177f4d67689, 0xfcff8783e0de9691, 0xffff7d84fce66c82, 0xe1e99b97e3ee846a,
-        0x222c31301a004d4a, 0x554a0d25371c3b43, 0x696100014b341d0e, 0x7b6e0005593b0c00,
-        0x8175052367452606, 0x7d7a384770455332, 0x66694e4d6c313d22, 0x2d215a722f005b00,
-        0x0060004100600029, 0x00e600de00c200bd, 0x032c0286019500ff, 0xffecfff6ffecfff9,
-        0xfffb000300030001, 0xfff3fffffff4fff0, 0x00000000fff9000b,
+        0x0000000000000000, 0x2c2d0f10312d1600, 0x221e211c29211a0c, 0x152030202b251b0e,
+        0x232d3a2b37382c1a, 0x51595a4e6b623d2c, 0x8c9b8770aba8676a, 0x0000000000000000,
+        0x0000000000000000, 0x3728022b291b2f0e, 0x2622111e21183217, 0x1a1d2027291d2215,
+        0x232d30383932301c, 0x57604a5980683a28, 0x96c17454d8c61034, 0x0000000000000000,
+        0x443c312f1e122424, 0x574c3a424738322e, 0x6b5e4e46543d462f, 0x747256525c4d5040,
+        0x7970676c674f5454, 0x6064928453427852, 0x594c70734e364842, 0x47574f0047000000,
+        0x4f493e394e414240, 0x5d574a5753445752, 0x6d6856555c58594a, 0x6c6c675865505250,
+        0x6e67716b6c585950, 0x5d678177615b735d, 0x605e5a5b604d5746, 0x69671b155d5f2a26,
+        0xb6ba746db8b36562, 0xb7b2676ab0b3604f, 0xbcbc6462b7b76a5b, 0xc4c66d69c5c26361,
+        0xc6cb837fcbc97771, 0xc2c7a197c5c99781, 0xc9caaba9cfc88a91, 0xc9caabaccacaa29e,
+        0x9f9cd7d3a0b1d1ca, 0xb2a4dfe3a2a3ded7, 0xd1d4dadec9b3dfdd, 0xfbeed8dae1cedfdc,
+        0xfffde1e4f6ccdded, 0xfdfff7f2ded1fdf5, 0xfffde7eef6d6d4e6, 0xe0e5fefbdfe9e2c3,
+        0x111f3d3d16006066, 0x483c0f302f1e4858, 0x575000014033251b, 0x625c00024e3b0c00,
+        0x6862011c5c42260d, 0x6b682b3f6544543a, 0x5b5f414663313e33, 0x271f53692c005d00,
+        0x0065003900650025, 0x00fa00de00df00ce, 0x037c022d01890107, 0xffebfff6ffebfffa,
+        0xfffc000500030002, 0xffedfffefff1ffea, 0x00000000fffd000d,
     };
 
     int EvalWeight(int item) => (int)(packedData[item / 2] >> item % 2 * 32);
@@ -92,7 +92,7 @@ public class MyBot : IChessBot {
             inQSearch = depth <= 0,
             pieceIsWhite;
         int
-            eval = 0x0008000a, // tempo
+            eval = 0x0009000a, // tempo
             bestScore = -99999,
             oldAlpha = alpha,
 
@@ -137,10 +137,6 @@ public class MyBot : IChessBot {
                                 packedData[pieceType * 8 + square.Rank ^ (pieceIsWhite ? 0 : 0b111)]
                                     >> (0x01455410 >> square.File * 4) * 8
                                     & 0xFF00FF
-                            )
-                            // mobility
-                            + EvalWeight(11 + pieceType) * GetNumberOfSetBits(
-                                GetSliderAttacks((PieceType)Min(5, pieceType), square, board)
                             )
                             // own pawn on file
                             + EvalWeight(118 + pieceType) * GetNumberOfSetBits(

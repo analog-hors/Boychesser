f10e595bfe9b1e3cf9ed5ca2e1820693b7588870
MinusKelvin <mark.carlson@minuskelvin.net>
2023-09-05 13:51:41 +1000

    dumb aspiration windows
    bench: 4732737
    tokens: 1054

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,7 +7,7 @@ public class MyBot : IChessBot {
     public int maxDepth = 999; // #DEBUG
 
     public long nodes = 0; // #DEBUG
-    public int maxSearchTime, searchingDepth;
+    public int maxSearchTime, searchingDepth, lastScore, score;
 
     public Timer timer;
     public Board board;
@@ -56,8 +56,11 @@ public class MyBot : IChessBot {
         do
             //If score is of this value search has been aborted, DO NOT use result
             try {
-                Negamax(-32000, 32000, searchingDepth);
+                score = Negamax(lastScore - 50, lastScore + 50, searchingDepth);
+                if (score <= lastScore - 50 || score >= lastScore + 50)
+                    score = Negamax(-32000, 32000, searchingDepth);
                 rootBestMove = searchBestMove;
+                lastScore = score;
                 //Use for debugging, commented out because it saves a LOT of tokens!!
                 //Console.WriteLine("info depth " + depth + " score cp " + score);
             } catch (TimeoutException) {

481beec44614a9e45d538f8b6f99733bb6f93ae9
analog-hors <44050761+analog-hors@users.noreply.github.com>
2023-09-28 21:12:51 +1000

    reintroduce quiet underpromotion ordering
    bench: 4789112
    tokens: 1030

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -188,11 +188,12 @@ public class MyBot : IChessBot {
             // move ordering:
             // 1. hashmove
             // 2. captures (ordered by MVV-LVA)
-            // 3. quiets (ordered by history)
+            // 3. quiets (no underpromotions, ordered by history)
+            // 4. underpromotion quiets (ordered by knight, bishop, rook, tiebreak by history)
             scores[tmp++] -= ttHit && move.RawValue == ttMoveRaw ? 1000000
                 : Max(
                     (int)move.CapturePieceType * 32768 - (int)move.MovePieceType - 16384,
-                    HistoryValue(move)
+                    HistoryValue(move) - (int)move.PromotionPieceType % 5 * 2048
                 );
         // end tmp use
 

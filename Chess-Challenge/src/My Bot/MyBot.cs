6f5fb3bc0ae51c5e7754e8865299663e8a218e46
MinusKelvin <mark.carlson@minuskelvin.net>
2023-09-30 21:57:31 +1000

    remove iir to fit
    bench: 4409484
    tokens: 1021

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -98,17 +98,13 @@ public class MyBot : IChessBot {
 
             // temp vars
             tmp = 0;
-        if (ttHit) {
-            if (ttDepth >= depth && ttBound switch {
-                2147483647 /* BOUND_LOWER */ => score >= beta,
-                0 /* BOUND_UPPER */ => score <= alpha,
-                // exact cutoffs at pv nodes causes problems, but need it in qsearch for matefinding
-                _ /* BOUND_EXACT */ => nonPv || inQSearch,
-            })
-                return score;
-        } else if (depth > 3)
-            // Internal iterative reduction
-            depth--;
+        if (ttHit && ttDepth >= depth && ttBound switch {
+            2147483647 /* BOUND_LOWER */ => score >= beta,
+            0 /* BOUND_UPPER */ => score <= alpha,
+            // exact cutoffs at pv nodes causes problems, but need it in qsearch for matefinding
+            _ /* BOUND_EXACT */ => nonPv || inQSearch,
+        })
+            return score;
 
         // this is a local function because the C# JIT doesn't optimize very large functions well
         // we do packed phased evaluation, so weights are of the form (eg << 16) + mg
